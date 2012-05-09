c1b803f283699b98b4d7815275bb6904bb72f9f7
yt0907.Wang@gmail.com <yt0907.Wang@gmail.com>
2012-05-09 17:26:43 +0800

    change Makefile

--- Server/include/threadpool/MiddleServer.h
+++ Server/include/threadpool/MiddleServer.h
@@ -8,9 +8,35 @@
  */
 
 #include "Agent.h"
+class Task;
 
 class MiddleServer:public Agent {
+ protected:
+    Agent * m_pAgent;
+    Task * m_pTask;
+    
  public:
+ 	MiddleServer(Agent *pAgent,Task *pTask=NULL):m_pAgent(pAgent),m_pTask(pTask)
+    {}
+ 	MiddleServer():m_pAgent(NULL),mpTask(NULL)
+    {}
+    
+    void setHandler(Task *pTask) {
+        m_pTask = pTask;
+    }
+
+    Task *getHandler(void){
+        return m_pTask;
+    }
+    
+    void setAgent(Agent *pAgent) {
+        m_pAgent = pAgent;
+    }
+    
+    Agent* getAgent(void) {
+        return m_pAgent;
+    }
+    
     virtual int putTaskFromAgent(unsigned long)
     {
         return SUCCESSFUL;
